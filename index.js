10bda30fd35dc303ddfd7e84a81b7a3410d90e94
S-Karthick-Raja <91246056+S-Karthick-Raja@users.noreply.github.com>
2022-02-02 01:00:07 +0530

    cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const  error  = require('./middlewares/errorMiddlewares-Handler');
 const usersRoute = require('./Routes/usersRoute');
 const dotenv = require('dotenv');
 const bookRouter = require('./Routes/bookRoute');
+const cors = require("cors");
 
 dotenv.config();
 
@@ -29,7 +30,10 @@ app.use(error.errorMiddlewareHandler);
 
 // SERVER
 const PORT = process.env.PORT || 9000 ;
-app.get("/", (req, res) => {
+app.get("/no-cors", (req, res) => {
   res.send("HELLO WORLD WELCOME");
 });
+
+
+
 app.listen(PORT, () => console.log("App is Started", PORT));
